63c54f487f7a743c5d71ba63377458184cec13ef
malairaja-mahadevan <37778244+malairaja-mahadevan@users.noreply.github.com>
2023-07-08 22:19:02 +0530

    Class, Generators

--- index.js
+++ index.js
@@ -107,3 +107,86 @@ let rabbit = new Rabbit("White Rabbit");
 
 rabbit.run(5);
 rabbit.stop();
+
+/* class inheritance for static properties and methods */
+class Animal {
+  static planet = "Earth";
+
+  constructor(name, speed) {
+    this.speed = speed;
+    this.name = name;
+  }
+
+  run(speed = 0) {
+    this.speed += speed;
+    console.log(`${this.name} runs with speed ${this.speed}.`);
+  }
+
+  static compare(animalA, animalB) {
+    return animalA.speed - animalB.speed;
+  }
+
+}
+
+class Rabbit extends Animal {
+  hide() {
+    alert(`${this.name} hides!`);
+  }
+}
+
+let rabbits = [
+  new Rabbit("White Rabbit", 10),
+  new Rabbit("Black Rabbit", 5)
+];
+
+rabbits.sort(Rabbit.compare);
+
+rabbits[0].run(); // Black Rabbit runs with speed 5.
+
+conosole.log(Rabbit.planet); // Earth
+
+/* Promise */
+let promiseToCleanRoom = new Promise(function(resolve, reject) {
+    let isClean = true;
+    
+    if(isClean) {
+        resolve('Clean');
+    } else {
+        reject('Bad');
+    }
+});
+
+promiseToCleanRoom
+.then((result) => console.log('The room is ' + result))
+.catch((result) => console.log('The room is not clean: ' + result));
+
+/* Async & Await */
+async function test() {
+  let newPromise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Promise is finished!"), 1000)
+  });
+  let promiseResult = await newPromise; 
+  console.log(promiseResult); // "done!"
+}
+test();
+
+/* Generator function */
+function* generateSequence() {
+  yield 1;
+  yield 2;
+  return 3;
+}
+
+let generator = generateSequence();
+
+let one = generator.next();
+
+console.log(JSON.stringify(one)); // {value: 1, done: false}
+
+let two = generator.next();
+
+console.log(JSON.stringify(two)); // {value: 2, done: false}
+
+let three = generator.next();
+
+console.log(JSON.stringify(three)); // {value: 3, done: true}
